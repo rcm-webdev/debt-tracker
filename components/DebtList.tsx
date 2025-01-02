1091768c4494a7c61f2a72aeb814380e8962e91b
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-01-01 17:37:27 -0800

    add wallet icon

--- components/DebtList.tsx
+++ components/DebtList.tsx
@@ -1,4 +1,25 @@
 export const DebtList = () => {
+  const icons = {
+    wallet: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+      >
+        <g
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+        >
+          <path d="M17 8V5a1 1 0 0 0-1-1H6a2 2 0 0 0 0 4h12a1 1 0 0 1 1 1v3m0 4v3a1 1 0 0 1-1 1H6a2 2 0 0 1-2-2V6" />
+          <path d="M20 12v4h-4a2 2 0 0 1 0-4z" />
+        </g>
+      </svg>
+    ),
+  };
   return (
     <div className="card bg-neutral w-full shadow-xl">
       <div className="card-body">
@@ -39,7 +60,7 @@ export const DebtList = () => {
                 <th></th>
                 <th>Type</th>
                 <th>Amount</th>
-                <th>Pay</th>
+                <th>Pay off debt</th>
               </tr>
             </thead>
             <tbody>
@@ -47,27 +68,28 @@ export const DebtList = () => {
               <tr className="bg-base-200">
                 <th>1</th>
                 <td>Credit Card</td>
-                <td className="text-error">₩5,000,000</td>
+                <td className="text-error">₩5,000,000,000</td>
                 <td>
-                  <button className="btn btn-primary">Pay</button>
+                  <button className="btn btn-primary">{icons.wallet}</button>
                 </td>
               </tr>
               {/* row 2 */}
               <tr>
                 <th>2</th>
-                <td>Car Loan</td>
-                <td className="text-error">₩600,000</td>
+                <td>Mortgage</td>
+                <td className="text-error">₩40,000,000,000</td>
                 <td>
-                  <button className="btn btn-primary">Pay</button>
+                  <button className="btn btn-primary">{icons.wallet}</button>
                 </td>
               </tr>
+
               {/* row 3 */}
               <tr>
                 <th>3</th>
-                <td>Mortgage</td>
-                <td className="text-error">₩40,000,000</td>
+                <td>Car Loan</td>
+                <td className="text-error">₩600,000,000</td>
                 <td>
-                  <button className="btn btn-primary">Pay</button>
+                  <button className="btn btn-primary">{icons.wallet}</button>
                 </td>
               </tr>
             </tbody>
